0aed3aa0443ccfae20f689b9c7a9e4fcc0bf3869
1Bronze <khdkkhdd@gmail.com>
2023-11-04 21:53:09 +0900

    style: mainpage, detailpage 머지로 인한 일부 스타일 수정

--- src/pages/MainPage/mainPage.js
+++ src/pages/MainPage/mainPage.js
@@ -1,17 +1,11 @@
-import React, { useState } from "react";
-import Header from "components/Header/header";
-import Logo from "assets/images/logo.png";
-import SearchBar from "components/Header/searchBar";
-import Dropdown from "components/Header/dropdowns";
-import ServiceIntro from "components/main/serviceIntro";
-import Mainheder from "components/Header/mainheader";
+import ServiceIntro from "pages/MainPage/serviceIntro";
+import MainHeader from "pages/MainPage/mainHeader";
 
 function MainPage({ searchOptions }) {
-  const [selectedOption, setSelectedOption] = useState(searchOptions[0]);
 
   return (
     <div className="flex flex-col items-center pb-32 min-h-screen">
-      <Mainheder></Mainheder>
+      <MainHeader searchOptions={searchOptions}></MainHeader>
       <ServiceIntro />
     </div>
   );
